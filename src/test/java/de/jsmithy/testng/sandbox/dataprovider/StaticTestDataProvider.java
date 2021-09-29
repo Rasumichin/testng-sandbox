0309e7626639c5b14f07c00157416f5c1ab7c120
Erik Lotz <it57@canda.com>
2021-09-29 17:01:29 +0200

    Documentation. Provided Javadoc.

--- src/test/java/de/jsmithy/testng/sandbox/dataprovider/StaticTestDataProvider.java
+++ src/test/java/de/jsmithy/testng/sandbox/dataprovider/StaticTestDataProvider.java
@@ -2,8 +2,21 @@ package de.jsmithy.testng.sandbox.dataprovider;
 
 import org.testng.annotations.DataProvider;
 
+/**
+ * A simple DataProvider class conforming to TestNG.
+ * 
+ * Refer to: https://testng.org/doc/documentation-main.html#parameters-dataproviders
+ * 
+ * @author Erik Lotz
+ * 
+ */
 public class StaticTestDataProvider {
 
+	/**
+	 * Provides a simple Object structure used for test parameterization. The static structure is
+	 * <code>{ {"CMO", "ELO"}, {"AGA", "RBRi"} }</code>.
+	 * 
+	 */
 	@DataProvider(name = "chaining-test-values")
 	public static Object[][] provideChainingTestValues() {
 		// @formatter:off
